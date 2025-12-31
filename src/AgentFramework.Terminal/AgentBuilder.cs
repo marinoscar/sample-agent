4d4fb4d13d4b49e4ab6f1c71a3a8fd9930f5ea9e
Oscar Marin <oscar@marin.cr>
2025-12-30 19:40:11 -0600

    Add agent factory initialization to AgentBuilder
    Introduces the InitializeAgentFactory extension method to register AgentStoreService and AgentFactory with dependency injection in HostApplicationBuilder. This streamlines agent-related service setup for terminal applications.

--- src/AgentFramework.Terminal/AgentBuilder.cs
+++ src/AgentFramework.Terminal/AgentBuilder.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AgentFramework.Core.Agents;
+using AgentFramework.Core.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +15,26 @@ namespace AgentFramework.Terminal
     /// </summary>
     public static class AgentBuilder
     {
+        public static HostApplicationBuilder InitializeAgentFactory(this HostApplicationBuilder builder)
+        {
+            // Register the AgentStoreService factory
+            builder.Services.AddScoped<Func<IAgentStore>>((c) =>
+            {
+                return () =>
+                {
+                    return new AgentStoreService(() => new SqliteAgentMessageContext());
+                };
+            });
 
+            // Register the AgentFactory with its dependencies
+            builder.Services.AddScoped<AgentFactory>(sp =>
+            {
+                var agentStoreFactory = sp.GetRequiredService<Func<IAgentStore>>();
+                var loggerFactory = sp.GetService<Microsoft.Extensions.Logging.ILoggerFactory>();
+                return new AgentFactory(agentStoreFactory, loggerFactory);
+            });
+
+            return builder;
+        }
     }
 }
