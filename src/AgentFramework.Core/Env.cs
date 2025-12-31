0eeafef4952420a9d3c52ac8d887834392a51372
Oscar Marin <oscar@marin.cr>
2025-12-30 19:32:08 -0600

    Remove AgentFactory and add XML docs to Env methods
    Deleted the unused AgentFactory class. Added XML documentation comments to Env.cs methods to clarify their usage and behavior.

--- src/AgentFramework.Core/Env.cs
+++ src/AgentFramework.Core/Env.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 
 namespace AgentFramework.Core
 {
+    /// <summary>
+    /// Provides utility methods for accessing environment variables.
+    /// </summary>
     public static class Env
     {
+        /// <summary>
+        /// Gets a required environment variable value.
+        /// </summary>
+        /// <param name="key">The name of the environment variable to retrieve.</param>
+        /// <returns>The trimmed value of the environment variable.</returns>
+        /// <exception cref="InvalidOperationException">Thrown when the environment variable is not set or is whitespace.</exception>
+        /// <remarks>
+        /// Searches for the environment variable in the following order:
+        /// <list type="number">
+        /// <item>Process-level variables</item>
+        /// <item>User-level variables</item>
+        /// <item>Machine-level variables</item>
+        /// </list>
+        /// </remarks>
         public static string GetRequired(string key)
         {
             var value =
@@ -21,6 +38,19 @@ namespace AgentFramework.Core
             return value.Trim();
         }
 
+        /// <summary>
+        /// Gets an optional environment variable value.
+        /// </summary>
+        /// <param name="key">The name of the environment variable to retrieve.</param>
+        /// <returns>The value of the environment variable, or <c>null</c> if not found.</returns>
+        /// <remarks>
+        /// Searches for the environment variable in the following order:
+        /// <list type="number">
+        /// <item>Process-level variables</item>
+        /// <item>User-level variables</item>
+        /// <item>Machine-level variables</item>
+        /// </list>
+        /// </remarks>
         public static string? GetOptional(string key)
         {
             return
